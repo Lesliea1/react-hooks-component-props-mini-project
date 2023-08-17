664215cfa8be58fca700254c955c0f319585bad3
leslie <alvarado.leslie12@yahoo.com>
2023-08-17 11:09:48 -0700

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
 function App() {
+  const blogName = "React to me";
+  const logoImage = "https://via.placeholder.com/215";
+  const aboutText = "Welcome to my introduction on React!";
+  const posts = [
+    {
+      id: 1,
+      title: "Getting Started with React",
+      date: "August 17, 2023",
+      preview:
+        "Learn the basics of React and how to set up your first project.",
+    },
+  ];
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogName} />
+      <About image={logoImage} about={aboutText} />
+      <ArticleList posts={posts} />
     </div>
   );
 }
